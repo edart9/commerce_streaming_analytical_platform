978e8ea3287e09e08ffc6674b687c1619cb19314
Enzo Arata <enzodanielarata@gmail.com>
2025-07-07 23:34:49 -0400

    building a basic pyflink consumer for kafka simulator with .jars dependencies

--- consumer_flink/flink_kafka_testing.py
+++ consumer_flink/flink_kafka_testing.py
@@ -1,56 +1,52 @@
-from pyflink.common import WatermarkStrategy, Row
-from pyflink.common.serialization import Encoder
-from pyflink.common.typeinfo import Types
 from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.datastream.connectors.file_system import FileSink, OutputFileConfig
-from pyflink.datastream.connectors.number_seq import NumberSequenceSource
-from pyflink.datastream.functions import RuntimeContext, MapFunction
-from pyflink.datastream.state import ValueStateDescriptor
+from pyflink.table import StreamTableEnvironment,EnvironmentSettings
+import os
+
+#create streaming enviroment
+env=StreamExecutionEnvironment.get_execution_environment()
+settings=EnvironmentSettings.new_instance()\
+                .in_streaming_mode()\
+                .build()
+
+#create table enviroment
+table_env=StreamTableEnvironment.create(stream_execution_environment=env,
+                                        environment_settings=settings)
+lib_path = os.path.join(os.path.dirname(__file__), "lib")
+jars = [f"file://{os.path.join(lib_path, jar)}" for jar in os.listdir(lib_path) if jar.endswith(".jar")]
+table_env.get_config().get_configuration().set_string("pipeline.jars", ";".join(jars))
+print(table_env)
 
 
-class MyMapFunction(MapFunction):
+#Creating kafka source table
+source="""
+        CREATE TABLE ecommerce(
+            event_time TIMESTAMP(3),
+            event_type varchar,
+            product_id bigint,
+            category_id bigint,
+            category_code varchar,
+            brand varchar,
+            price double,
+            user_id bigint,
+            user_session varchar,
+            ip varchar,
+            proctime as PROCTIME()
+        ) with (
+            'connector'='kafka',
+            'topic'='ecommerce_online_events',
+            'properties.bootstrap.servers'='kafka:9092',
+            'properties.group.id'='ecommerce',
+            'scan.startup.mode' = 'earliest-offset',
+            'format'='json'
+        )
+"""
+table_env.execute_sql(source)
+table=table_env.from_path('ecommerce')
+print ('\nSource Schema')
+table.print_schema()
+table_env.execute_sql("SELECT * FROM ecommerce").print()
 
-    def open(self, runtime_context: RuntimeContext):
-        state_desc = ValueStateDescriptor('cnt', Types.PICKLED_BYTE_ARRAY())
-        self.cnt_state = runtime_context.get_state(state_desc)
-
-    def map(self, value):
-        cnt = self.cnt_state.value()
-        if cnt is None or cnt < 2:
-            self.cnt_state.update(1 if cnt is None else cnt + 1)
-            return value[0], value[1] + 1
-        else:
-            return value[0], value[1]
-
-
-def state_access_demo():
-    # 1. create a StreamExecutionEnvironment
-    env = StreamExecutionEnvironment.get_execution_environment()
-
-    # 2. create source DataStream
-    seq_num_source = NumberSequenceSource(1, 10000)
-    ds = env.from_source(
-        source=seq_num_source,
-        watermark_strategy=WatermarkStrategy.for_monotonous_timestamps(),
-        source_name='seq_num_source',
-        type_info=Types.LONG())
-
-    # 3. define the execution logic
-    ds = ds.map(lambda a: Row(a % 4, 1), output_type=Types.ROW([Types.LONG(), Types.LONG()])) \
-           .key_by(lambda a: a[0]) \
-           .map(MyMapFunction(), output_type=Types.TUPLE([Types.LONG(), Types.LONG()]))
-
-    # 4. create sink and emit result to sink
-    output_path = '/opt/output/'
-    file_sink = FileSink \
-        .for_row_format(output_path, Encoder.simple_string_encoder()) \
-        .with_output_file_config(OutputFileConfig.builder().with_part_prefix('pre').with_part_suffix('suf').build()) \
-        .build()
-    ds.sink_to(file_sink)
-
-    # 5. execute the job
-    env.execute('state_access_demo')
-
-
-if __name__ == '__main__':
-    state_access_demo()
+# #Calculation
+# sql="""
+#     select 
+# """
