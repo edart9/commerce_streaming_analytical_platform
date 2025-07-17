d44606aff943c01d6c223881455c9e949d9c75d9
Enzo Arata <enzodanielarata@gmail.com>
2025-07-16 23:32:19 -0400

    Change Flink container structure to simulate a real Flink cluster

--- consumer_flink/consumer_flink.py
+++ consumer_flink/consumer_flink.py
@@ -2,13 +2,13 @@ from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.table import StreamTableEnvironment,EnvironmentSettings
 import os
 
-#create streaming enviroment
+
 env=StreamExecutionEnvironment.get_execution_environment()
 settings=EnvironmentSettings.new_instance()\
                 .in_streaming_mode()\
                 .build()
 
-#create table enviroment
+
 table_env=StreamTableEnvironment.create(stream_execution_environment=env,
                                         environment_settings=settings)
 lib_path = os.path.join(os.path.dirname(__file__), "lib")
@@ -17,7 +17,7 @@ table_env.get_config().get_configuration().set_string("pipeline.jars", ";".join(
 print(table_env)
 
 
-#Creating kafka source table
+
 source="""
         CREATE TABLE ecommerce(
             event_time TIMESTAMP(3),
@@ -40,33 +40,19 @@ source="""
             'format'='json'
         )
 """
-# Execute and confirm
+
 try:
     table_env.execute_sql(source)
     print("✅ Source table created")
 except Exception as e:
     print("❌ Table creation failed:", e)
 
-# Skip from_path() unless you need Table API access
+
 query = """
 SELECT *
-FROM (
-    SELECT *,
-           ROW_NUMBER() OVER (PARTITION BY window_start ORDER BY total_revenue DESC) AS rownum
-    FROM (
-        SELECT
-            TUMBLE_START(proctime, INTERVAL '1' MINUTE) AS window_start,
-            brand,
-            product_id,
-            SUM(price) AS total_revenue
-        FROM ecommerce
-        WHERE event_type = 'purchase'
-        GROUP BY TUMBLE(proctime, INTERVAL '1' MINUTE), brand, product_id
-    )
-)
-WHERE rownum <= 5
+FROM ecommerce
 """
 
-# Execute SQL query
+
 print("✅ Executing query:")
 table_env.execute_sql(query).print()
